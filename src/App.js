b5cbecc7b51ae83ef40d7b72413bfc519c1e9bea
sayin47 <sayinmehmet47@gmail.com>
2021-04-15 12:39:42 +0300

    quote machine now sent twit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [text, setText] = useState([]);
+  const [author, setAuthor] = useState([]);
+
+  const getQuote = () => {
+    const random = Math.floor(Math.random() * 1643 + 1);
+    fetch('https://type.fit/api/quotes')
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setText(result[random].text);
+          setAuthor(result[random].author);
+        },
+
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  };
+
+  useEffect(() => {
+    getQuote();
+  }, []);
+
+
+const redirect=()=>{
+
+  window.location.href= 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text='+encodeURIComponent('"' + text + '" ' + author)
+  ;
+
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="d-flex flex-column  border border-primary w-50 shadow container"
+      id="quote-box"
+    >
+      <h2 id="text" className="m-4">
+        {text}
+      </h2>
+      <h4 id="author" className="text-info text-end m-3">
+        {author}
+      </h4>
+      <button id="new-quote" className="btn btn-primary" onClick={getQuote}>
+        New Quote
+      </button>
+
+      <a onClick={redirect}
+ id="tweet-quote" className="mt-5">
+        <i className="fab fa-twitter fa-1x"></i>
+      </a>
     </div>
   );
 }
